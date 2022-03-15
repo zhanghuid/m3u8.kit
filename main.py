5f5959a4301b566989ae1d22a9f0c3d6db1a3765
zhanghuide <zhanghuide@aidalan.com>
2022-03-15 21:04:28 +0800

    feat: init

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from bottle import route, run, error, response, hook, request
+from util import *
+from dump_har import get_m3u8
+
+_allow_origin = '*'
+_allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
+_allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+
+@hook('after_request')
+def enable_cors():
+    '''Add headers to enable CORS'''
+    response.headers['Access-Control-Allow-Origin'] = _allow_origin
+    response.headers['Access-Control-Allow-Methods'] = _allow_methods
+    response.headers['Access-Control-Allow-Headers'] = _allow_headers\
+
+
+@route('/m3u8')
+def m3u8():
+    url = request.query.url
+    if not url:
+        return fail("url is empty")
+    data = get_m3u8(url)
+    return success(data)
+
+
+@error(404)
+def error404(error):
+    return fail("404")
+
+
+@error(405)
+def error404(error):
+    return fail("405")
+
+
+run(host='localhost', port=8078, debug=True)
